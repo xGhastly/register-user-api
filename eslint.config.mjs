0f16054d31aaae527a063f84c4cfa2d9a0d645ac
João Augusto <joaoaugustozm@hotmail.com>
2024-09-24 17:35:11 -0300

    att

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'commonjs' },
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'warn', // Erro se Prettier não respeitar as regras
+    },
+  },
+  {
+    files: ['eslint.config.mjs'], // Regras mais permissivas para arquivos de configuração
+    rules: {
+      'prettier/prettier': 'off', // Desativa Prettier para o arquivo de configuração
+    },
+  },
+];
